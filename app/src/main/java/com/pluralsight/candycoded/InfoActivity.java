2ec93bc774425a7b724bb1cf1533db08ba7b46ba
joykangangi <64706463+joykangangi@users.noreply.github.com>
2021-12-02 17:54:25 +0300

    Explicit intent to open a map in Info activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,5 +1,6 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.net.Uri;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+import android.view.View;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -24,9 +26,18 @@ public class InfoActivity extends AppCompatActivity {
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
-
-
     }
+
+    //method to attach to the Click Listener on the TextView
+    public void createMapIntent(View view) {
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801"); //uri for the store
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
